c8bee43c6774c9fab1e3e3a4854943d651c33c40
Uelber Henrique <uelber2011@gmail.com>
2019-08-28 12:14:12 -0300

    Login Screen, Front

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="PT-BR">
+
+<head>
+    <meta charset="utf-8"/>
+    <title> Projeto Login </title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<div id="corpo-form"> 
+    <h1> Entrar </h1>
+    
+    <form method="POST" action="processa.php">
+
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="submit" value="Acessar">
+        <a href="">>Ainda não é Inscrito? <strong>Cadastre-se!</strong></a>
+
+    </form>
+</div>
+<body>
+    
+</body>
+
+
+</html>
